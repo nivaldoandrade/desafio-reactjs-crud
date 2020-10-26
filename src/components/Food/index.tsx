79499731a1d40d88eecb7dde5c9ff90452665713
Nivaldo Andrade <nivaldoandradef@gmail.com>
2020-10-26 17:00:39 -0300

    add: updating food availability on API

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { FiEdit3, FiTrash } from 'react-icons/fi';
+import api from '../../services/api';
 
 import { Container } from './styles';
 
@@ -28,6 +29,12 @@ const Food: React.FC<IProps> = ({
 
   async function toggleAvailable(): Promise<void> {
     // TODO UPDATE STATUS (available)
+
+    await api.put(`foods/${food.id}`, {
+      ...food,
+      available: !isAvailable,
+    });
+
     setIsAvailable(!isAvailable);
   }
 
